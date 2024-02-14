4f5e5d311a3aa227dcc6b8f93ea6978a6440d360
Mila <mia.denis@yandex.ru>
2024-02-14 19:34:34 +0300

    add lesson1:task4

---
+++ Lesson1/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// 5 гирь с разным весом, найти самую тяжелую
+int a = 6;
+int b = 7;
+int c = 9;
+int d = 4;
+int e = 10;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
