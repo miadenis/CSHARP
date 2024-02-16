e2f2ac76c5d1df8dd6a87804f7d0395567610bdb
Mila <mia.denis@yandex.ru>
2024-02-16 09:29:09 +0300

    add seminar1:task1

---
+++ Seminar1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая на вход принимает два целых числа и проверяет,
+// является ли первое число квадратом второго.
+
+
+
+Console.WriteLine("num");
+string var1 = Console.ReadLine();
+int var_num1 = int.Parse(var1);
+
+Console.WriteLine("num2");
+string var2 = Console.ReadLine();
+int var_num2 =int.Parse(var2);
+if (var_num1 == var_num2 * var_num2)
+    Console.WriteLine("Yes");
+else
+    Console.WriteLine("No");
