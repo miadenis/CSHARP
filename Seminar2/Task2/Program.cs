ee843cbae2c807fb63b4108feffc79a67cb64b09
Mila <mia.denis@yandex.ru>
2024-02-22 09:35:25 +0300

    add Seminar2:task2

---
+++ Seminar2/Task2/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число и возводит вторую цифру 
+//этого числа в степень, равную третьей цифре
+
+int num1 = int.Parse(Console.ReadLine()!);
+int num3 = num1%10;
+int num2 = (num1/10)%10;
+Console.WriteLine(Math.Pow(num2,num3));
