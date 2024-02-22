efe7978d23cf7e176d602e535202a823428ee41c
Mila <mia.denis@yandex.ru>
2024-02-22 11:25:25 +0300

    add Lesson2:task1

---
+++ Lesson2/Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// 
+int[] arr = new int[5]; //команда выделения памяти
+arr[0] = 5;
+arr[1] = 7;
+arr[2] = 5;
+arr[3] = 4;
+arr[4] = 3;
+Console.WriteLine(arr[0]);
+Console.WriteLine(arr[1]);
+Console.WriteLine(arr[2]);
+Console.WriteLine(arr[3]);
+Console.WriteLine(arr[4]);
+
+//Console.WriteLine(arr[5]); так нельзя, ошибка:Index was outside the bounds of the array
+
+int[] arr2 = new int[5] { 1, 2, 3, 4, 5 }; //сразу указываем числа массива
+
+int[] arr3 = { 1, 2, 3, 4, 5 }; // сокращаем, не используем команду выделения памяти
+
+//arr = [1,2,3,4,5] на Python
+
