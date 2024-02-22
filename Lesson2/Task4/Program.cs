7c41699ba288a9473f652029c036665afc7d8e7a
Mila <mia.denis@yandex.ru>
2024-02-22 15:48:47 +0300

    add task4:for,foreach

--- Lesson2/Task4/Program.cs
+++ Lesson2/Task4/Program.cs
@@ -1,5 +1,7 @@
 ﻿// Дано N гирь с разным весом в случайном порядке. Определить вес самой тяжелой гири.
 
+// С циклом while
+
 int n = 5;
 int[] array = { 2, 4, 7, 3, 9 };
 int i = 0;
@@ -12,5 +14,29 @@ while(i < n)
         max = array[i];
     }
     i = i + 1;
+}
+Console.WriteLine(max);
+
+// C циклом for
+
+max = array[0];
+for(int j = 0; j < n; j++) //j++ тоже самое что и j = j + 1
+{
+   if (array[j] > max)
+    {
+        max = array[j];
+    }
+} 
+Console.WriteLine(max);
+
+// С циклом foreach
+
+max = array[0];
+foreach(int e in array) // e - имя переменной
+{
+   if (e > max)
+    {
+        max = e;
+    } 
 }
 Console.WriteLine(max);
