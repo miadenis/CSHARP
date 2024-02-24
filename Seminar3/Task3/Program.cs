e829424863b22d97bdeb46b71e4fbd307d309384
Mila <mia.denis@yandex.ru>
2024-02-24 11:47:35 +0300

    add Seminar3:task3

---
+++ Seminar3/Task3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Найдите произведения пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+// Результат запишите в новый массив.
+
+int[] arr = new int[] { 5, 6, 8, 3, 10, 2 };
+int size = arr.Length;
+int[] arr2 = new int[size / 2 + size % 2]; // остаток от деления
+
+for (int i = 0; i < size / 2; i++)
+{
+    arr2[i] = arr[i] * arr[size - 1 - i];
+}
+if (size % 2 == 1)
+{
+    arr2[arr2.Length - 1] = arr[size / 2];
+}
+foreach (var item in arr2)
+{
+    Console.Write(item + " ");
+}
+
+
+
