effcc5a915bfdda689011c91a84f61ddb983db82
Mila <mia.denis@yandex.ru>
2024-02-24 17:30:07 +0300

    add Homework1:task1-4

---
+++ Homework1/Task1/Program.cs
@@ -0,0 +1,76 @@
+﻿// Внутри класса Answer напишите метод CompareNumbers, который на вход принимает два числа 
+//и выводит, какое число большее, а какое меньшее.
+
+
+
+using System;
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        // Введите свое решение ниже
+     
+        if (firstNumber > secondNumber)
+          Console.WriteLine("первое число больше");
+      
+        else if (firstNumber < secondNumber)
+            Console.WriteLine("второе число больше");
+        else
+        Console.WriteLine("числа равны");
+  
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            firstNumber = 6;
+            secondNumber = 3;
+        }
+
+        // Не удаляйте строки ниже
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
+
+//Решение автотеста:
+using System;
+
+
+public class Answer {
+    static void CompareNumbers(int firstNumber, int secondNumber)
+    {
+        if (firstNumber == secondNumber)
+        {
+            Console.WriteLine($"числа равны");
+        }
+        if (firstNumber > secondNumber)
+        {
+            Console.WriteLine($"первое число больше`");
+        }
+        if (firstNumber < secondNumber)
+        {
+            Console.WriteLine($"второе число больше");
+        }
+    }
+
+    static public void Main(string[] args) {
+        int firstNumber, secondNumber;
+
+        if (args.Length >= 2) {
+            firstNumber = int.Parse(args[0]);
+            secondNumber = int.Parse(args[1]);
+        } else {
+            firstNumber = 5;
+            secondNumber = 5;
+        }
+
+        CompareNumbers(firstNumber, secondNumber);
+    }
+}
---
+++ Homework1/Task4/Program.cs
@@ -0,0 +1,61 @@
+﻿// Внутри класса Answer напишите метод PrintEvenNumbers, 
+//которая на вход принимает число (number), 
+//а на выходе выводит все чётные числа от 1 до number (включительно), 
+//после каждого числа должен быть знак пробела.
+
+using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+      // Введите свое решение ниже
+  int i = 2;
+      while (i <= number)
+      {
+        Console.Write($"{i} ");
+        i += 2;
+      }
+
+    }
+
+
+  // Не удаляйте и не меняйте метод Main! 
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            number = 8;
+        }
+
+        // Не удаляйте строки ниже
+        PrintEvenNumbers(number);
+    }
+}
+
+//Решение автотеста:
+using System;
+
+public class Answer {
+    static void PrintEvenNumbers(int number)
+    {
+        for (int count = 2; count <= number; count += 2)
+        {
+            Console.Write($"{count} ");
+        }
+    }
+
+    static public void Main(string[] args) {
+        int number;
+
+        if (args.Length >= 1) {
+            number = int.Parse(args[0]);
+        } else {
+            number = 6;
+        }
+
+        PrintEvenNumbers(number);
+    }
+}
