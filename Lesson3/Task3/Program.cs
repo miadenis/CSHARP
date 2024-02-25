7c70351a80a98130076f7df2cedf555639d7aeb7
Mila <mia.denis@yandex.ru>
2024-02-25 10:38:44 +0300

    add Lesson3:task3

---
+++ Lesson3/Task3/Program.cs
@@ -0,0 +1,23 @@
+﻿// удаление четных чисел из массива
+
+void ZeroEvenElements(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+        {
+            arr[i] = 0;
+        }
+    }
+}
+
+void PrintArray(int[] arr)
+{
+    foreach (var e in arr)
+    {
+        Console.Write($"{e} ");
+    }
+}
+int[] array = {1, 2, 3, 4, 5};
+ZeroEvenElements(array);
+PrintArray(array);
